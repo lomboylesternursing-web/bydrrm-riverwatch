b8029fa9130d6d9806700560becfb569a9bf9ef9
lomboylesternursing-web <lomboylester.nursing@gmail.com>
2026-08-09 23:32:14 +0800

    Connect RiverWatch Firebase project

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.RIVERWATCH_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDaGhP0mMYnUkCPo48oRwB7YhcTTJxyjQU",
+  authDomain: "bydrrm-riverwatch.firebaseapp.com",
+  projectId: "bydrrm-riverwatch",
+  storageBucket: "bydrrm-riverwatch.firebasestorage.app",
+  messagingSenderId: "365444961445",
+  appId: "1:365444961445:web:2f274393e9eb29fdc3a94c",
+  measurementId: "G-E973Q4KPJZ"
+};
